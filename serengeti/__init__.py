03793aada69d78b2e771302b8299c369938d27d1
stkyle <stkyle@users.noreply.github.com>
2015-12-03 11:53:32 -0500

    Create __init__.py

---
+++ serengeti/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+__author__ = "Steve Kyle"
+
+__license__ = "GPL"
+__version__ = "1"
+__maintainer__ = "Steve Kyle"
+__email__ = " "
+__status__ = "Development"
+"""
+import sys
+import os
+
