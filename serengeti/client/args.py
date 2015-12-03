5315bdd5089df4564ab05e7b198a3f8490937041
stkyle <stkyle@users.noreply.github.com>
2015-12-03 14:06:51 -0500

    Update args.py

--- serengeti/client/args.py
+++ serengeti/client/args.py
@@ -1,71 +1,28 @@
-    def get_arg_parser(self,
-                       parser_description,
-                       path="/services/discovery/",
-                       host="taxiitest.mitre.org",
-                       port="80",
-                       https=False,
-                       cert=None,
-                       key=None,
-                       username=None,
-                       password=None,
-                       proxy='noproxy',
-                       xml_output=False):
-        """
-        Parser things common to all scripts. Parsers for specific TAXII Services should
-        add their own arguments.
-        """
-        parser = argparse.ArgumentParser(description=parser_description)
-        url = "https" if https else "http" + "://" + host + ":" + str(port) + path
-        parser.add_argument("-u",
-                            "--url",
-                            dest="url",
-                            default=url,
-                            help="The URL to connect to. Defaults to %s." % url)
-        parser.add_argument("--host",
-                            dest="host",
-                            default=host,
-                            help=argparse.SUPPRESS
-                            )
-        parser.add_argument("--port",
-                            dest="port",
-                            default=port,
-                            type=int,
-                            help=argparse.SUPPRESS)
-        parser.add_argument("--path",
-                            dest="path",
-                            default=path,
-                            help=argparse.SUPPRESS)
-        parser.add_argument("--https",
-                            dest="https",
-                            default=https,
-                            type=bool,
-                            help=argparse.SUPPRESS)
-        parser.add_argument("--cert",
-                            dest="cert",
-                            default=cert,
-                            help="The file location of the certificate to use. Defaults to %s." % cert)
-        parser.add_argument("--key",
-                            dest="key",
-                            default=key,
-                            help="The file location of the private key to use. Defaults to %s." % key)
-        parser.add_argument("--username",
-                            dest="username",
-                            default=username,
-                            help="The username to authenticate with. Defaults to %s." % username)
-        parser.add_argument("--pass",
-                            dest="password",
-                            default=password,
-                            help="The password to authenticate with. Defaults to %s." % password)
-        parser.add_argument("--proxy",
-                            dest="proxy",
-                            action=ProxyAction, default=proxy,
-                            help="The proxy to use (e.g., http://myproxy.example.com:80/), or 'noproxy' to not use "
-                                 "any proxy. If omitted, the system's proxy settings will be used.")
-        parser.add_argument("--xml-output",
-                            dest="xml_output",
-                            action='store_true',
-                            default=xml_output,
-                            help="If present, the raw XML of the response will be printed to standard out. "
-                                 "Otherwise, a \"Rich\" output will be presented.")
 
-        return parser
+
+import argparse
+
+class Option(object):
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+# Default Command Line Arguments
+CLI_ARGS  = [Option('-u', '--url', dest='url', help='XXX')]
+CLI_ARGS += [Option('-t', '--host', dest='host', help='XXX')]
+CLI_ARGS += [Option('-p', '--port', dest='port', help='XXX')]
+CLI_ARGS += [Option('-s', '--https', dest='https', help='XXX')]
+CLI_ARGS += [Option('-c', '--cert', dest='cert', help='XXX')]
+CLI_ARGS += [Option('-k', '--key', dest='key', help='XXX')]
+CLI_ARGS += [Option('--proxy', dest='proxy', help='XXX')]
+
+
+def get_arg_parser(parser_description = None, arg_list=None):
+    """
+
+    """
+    arg_list = arg_list or CLI_ARGS
+    parser = argparse.ArgumentParser(description=parser_description)
+    for option in arg_list:
+        parser.add_argument(*option.args, **option.kwargs)
+    return parser
