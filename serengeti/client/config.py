7d72f4ac7b8f33e8bf664e8c243356ba23b990e5
stkyle <stkyle@users.noreply.github.com>
2015-12-03 13:01:43 -0500

    Update config.py

--- serengeti/client/config.py
+++ serengeti/client/config.py
@@ -20,8 +20,9 @@ Configure Serengeti TAXII Client
 from __future__ import print_function
 import sys
 import os
+from collections import OrderedDict
+from ConfigParser import ConfigParser as _ConfigParser
 
-from ConfigParser import SafeConfigParser as ConfigParser
 CONF_FILE = '.serengeti.client'
 
 CONF_OPTIONS = {'CLIENT': { 'cert':None,
@@ -37,7 +38,37 @@ CONF_OPTIONS = {'CLIENT': { 'cert':None,
                             },
                 }
 
-config = ConfigParser()
+
+class ConfigParser(_ConfigParser):
+
+    # modified to accept file names as input
+    def write(self, fp):
+        """Write an .ini-format representation of the configuration state."""
+        if isinstance(fp, file):
+            _ConfigParser.write(self, fp)
+        elif isinstance(fp, basestring):
+            with open(fp, 'wb') as fpp:
+                _ConfigParser.write(self, fpp)
+        else:
+            raise TypeError('fp must be of type file or basestring')
+
+    def to_dict(self):
+        """Export ConfigParser Object to OrderedDict"""
+        d = OrderedDict.fromkeys(self.sections())
+        for section in d.keys():
+            d[section] = dict(self.items(section))
+        return d
+
+    @staticmethod
+    def from_dict(d):
+        """Import Dict to create new ConfigParser Object"""
+        c = ConfigParser()
+        for section in d.keys():
+            c.add_section(section)
+            for option, value in d[section].iteritems():
+                c.set(section, option, value)
+        return c
+
 
 if os.path.exists(CONF_FILE) is False:
     cfg_file = open(CONF_FILE, 'w')
